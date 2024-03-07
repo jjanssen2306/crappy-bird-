3c8b2cd58a1276198523c44b6b834fc12bae732a
hussein <118814567+husseinomar19@users.noreply.github.com>
2024-03-07 14:26:22 +0100

    Header en Footer gemaakt door Hussein

---
+++ index/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <a href="">Logo</a>
+
+    <ul>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+    </ul>
+    <ul>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+    </ul>
+</footer>
---
+++ index/header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <a href="">Logo</a>
+    </nav>
+
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Over Spel</a></li>
+        <li><a href="">Spel omgeving</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+</header>
